a7f78a3636c76f76278cc8ced5f14eb54ff48cdd
Szazidayum <szabrinamolnar@gmail.com>
2022-09-11 14:25:36 +0200

    metódusokba rendezés

--- src/main/Magyarkartya.java
+++ src/main/Magyarkartya.java
@@ -16,124 +16,94 @@ public class Magyarkartya {
     public static Scanner sc = new Scanner(System.in);
     static Random rnd = new Random();
     static String lap;
+    static int hossz;
     static int ujLap;
+    static String ujErtek;
+    static int kezdo;
     
     public static void main(String[] args) {
+    //a kevert megírása, hogy mindig a 11.lap a gondolt legyen! 3x bekérni kell a felhasználótól
+    //GRAFikusan a varázslósat
         String[] szinek = {"P", "T", "Z", "M"};
         String[] ertek = {"Ász", "Kir", "Fel", "X", "IX", "VIII"};
 
         for (int i = 0; i < szinek.length; i++) {
             for (int j = 0; j < ertek.length; j++) {
-               
-//                System.out.println(szinek[i]+ertek[j]);
                 lap = szinek[i]+ertek[j];
 //                System.out.println(lap);
             if(!("MX".equals(lap) || "MIX".equals(lap) || "MVIII".equals(lap))){
-                    pakli.add(lap);
-}
-                    
-                }
-        }
+                    pakli.add(lap);}  
+                }}
               System.out.println(pakli);  
+              hossz=pakli.size();
+
+            sorok();
+            randomKeveres();
+            ujPakliKezdo();
+            ujPakliKeszites();
+
+//        sor1.clear();
+//        sor2.clear();
+//        sor3.clear();
         
-              int hossz=pakli.size();
-              System.out.println(hossz);
-             for (int i = 0; i < hossz; i++) {
-                 if(i<7){
-                    sor1.add(pakli.get(i)); 
-                 }
-                
-                if(i>=7 && i<14){
-                    sor2.add(pakli.get(i));
-                }
-                if(i>=14){
-                    sor3.add(pakli.get(i));
-                }
-            }
-             kevertP.add(sor1);
-             kevertP.add(sor2);
-             kevertP.add(sor3);
-        System.out.println(sor1);
-        System.out.println(sor2);
-        System.out.println(sor3);
-   
-        
-
-
-//random keverés
-
-//        for (int i = 0; i < hossz; i++) {
-            ujLap = (int) ((Math.random() * (hossz - 0)) + 0);
-            String ujErtek = pakli.get(ujLap);
-//            System.out.println(ujLap);
+        System.out.println(pakli);
+        System.out.println(ujPakli);
+    }
+    
+     private static void sorok(){
+        for (int i = 0; i < hossz; i++) {
+                     if(i<7){
+                        sor1.add(pakli.get(i)); 
+                     }
+                    if(i>=7 && i<14){
+                        sor2.add(pakli.get(i));
+                    }
+                    if(i>=14){
+                        sor3.add(pakli.get(i));
+                    }}
+                 kevertP.add(sor1);
+                 kevertP.add(sor2);
+                 kevertP.add(sor3);
+            System.out.println(sor1);
+            System.out.println(sor2);
+            System.out.println(sor3);
+}
+     
+     private static String randomKeveres(){
+           ujLap = (int) ((Math.random() * (hossz - 0)) + 0);
+            ujErtek = pakli.get(ujLap);
             System.out.println(ujErtek);
-//            ujPakli.add(ujErtek);
-////            System.out.println("űj érték:"+ujErtek);
-//            pakli.remove(lap);
-////            System.out.println("régi pakli index:"+i);
-//            hossz=pakli.size();
-////            System.out.println("űúj pakli size:"+hossz);
-//        }
-//        System.out.println(ujPakli);
-//        System.out.println(ujPakli.get(0));
-        
-        
-        
-//        int ujHossz=ujPakli.size();
-        //új oszlopok kialakítása
-        boolean kiir=false;
+            return ujErtek;
+     }
+     
+   private static boolean ujPakliKezdo(){
+    boolean kiir=false;
         while(kiir==false) {
             if(kiir==false){
                 for (int t = 0; t < sor1.size(); t++) {
                 if(sor1.get(t).equals(ujErtek)){
-                    System.out.println("megvan!");
                     kiir=true;
             }else if(kiir==false){
                     for (int z = 0; z < sor2.size(); z++) {
                 if(sor2.get(z).equals(ujErtek)){
-                    System.out.println("megvan!");
                     kiir=true;
-                }else{
+                }else if(kiir==false){
                     for (int u = 0; u < sor3.size(); u++) {
                 if(sor3.get(u).equals(ujErtek)){
-                     System.out.println("megvan!");
                      kiir=true;
-                }
-                }
-            
-            
-            
-                }
-            }
-            
-            }
-            
-            }
-                
-        }
+                }}}}}}}
              }
-        sor1.clear();
-        sor2.clear();
-        sor3.clear();
-//                 if(k<7){
-//                    sor1.add(ujPakli.get(k)); 
-//                 }
-//                
-//                if(k>=7 && k<14){
-//                    sor2.add(ujPakli.get(k));
-//                }
-//                if(k>=14){
-//                    sor3.add(ujPakli.get(k));
-//                }
-//            }
-//             kevertP.add(sor1);
-//             kevertP.add(sor2);
-//             kevertP.add(sor3);
-//        System.out.println(sor1);
-//        System.out.println(sor2);
-        System.out.println(sor3);
-   
-    //a kevert megírása, hogy mindig a 11.lap a gondolt legyen! 3x bekérni kell a felhasználótól
-    //GRAFikusan a varázslósat
-    } 
+        
+        return kiir;
+}
+   private static void ujPakliKeszites(){
+//       System.out.println(ujLap);
+        for (int a = ujLap; a < hossz; a++) {
+               ujPakli.add(pakli.get(a));
+            }
+        
+        for (int k = 0; k < ujLap; k++) {
+            ujPakli.add(pakli.get(k));
+        }
+   }
 }
